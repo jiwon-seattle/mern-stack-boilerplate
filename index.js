c9582c822d37d5378f0c9e0165f65661f2570ffb
Jiwon <jiwon1.han@gmail.com>
2020-06-05 16:53:54 -0700

    body parser and registration post

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express') //fetching express module
 const app = express()
 const port = 5000
 
+const bodyparser = require('body-parser')
+const { User } = require('./models/user')
+
+//application/x-www-form-urlencoded
+app.use(bodyparser.urlencoded({extended: true}))
+
+//application/json
+app.use(bodyparser.json())
+
+
 const mongoose =require('mongoose')
 mongoose.connect('mongodb+srv://new-user:newuser1234@youtubeclone-ev8r3.mongodb.net/<dbname>?retryWrites=true&w=majority', {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
@@ -10,4 +20,13 @@ mongoose.connect('mongodb+srv://new-user:newuser1234@youtubeclone-ev8r3.mongodb.
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.post ('/register', (req, res) => {
+  
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return (res.json({ success: false, err }))
+    return res.status(200).json({ success: true})
+  })
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
