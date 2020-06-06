77d16289526511fdd7cc9914f050da466cff2fda
Jiwon <jiwon1.han@gmail.com>
2020-06-05 20:50:38 -0700

    add bcrpyt

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
-const mongoose = require('mongoose');
-
+const mongoose = require('mongoose')
+const bcrypt =require('bcrypt')
+const saltRounds = 10;
 const userSchema = mongoose.Schema({
   name: {
     type: String,
@@ -30,6 +31,24 @@ const userSchema = mongoose.Schema({
     type: Number
   }
 })
+
+userSchema.pre('save', function(next) {
+  var user = this;
+  if(user.isModified('password')){    
+    // console.log('password changed')
+    bcrypt.genSalt(saltRounds, function(err, salt){
+        if(err) return next(err);
+
+        bcrypt.hash(user.password, salt, function(err, hash){
+            if(err) return next(err);
+            user.password = hash 
+            next()
+        })
+    })
+} else {
+    next()
+}
+});
 
 const User = mongoose.model('User', userSchema)
 
